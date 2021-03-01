a90f8de0ecde37913e8a05f6a6b257c78768c1e9
Rafael Scholant <rafaelscholant@pop-os.localdomain>
2021-03-01 20:10:39 -0300

    📋 - Aula 56 - Estilizando um pouco nossa lista.

--- people/App.js
+++ people/App.js
@@ -1,21 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import Header from './src/components/Header';
+import PeopleList from './src/components/PeopleList';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import axios from 'axios';
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      peoples: [],
+    };
+  }
+  componentDidMount() {
+    axios
+    .get('https://randomuser.me/api/?nat=br&results=5')
+    .then(response => {
+      const { results } = response.data;
+      this.setState({
+        peoples: results,
+      })
+    });
+  }
+
+  render() {
+    return (
+      <View>
+        <Header title="Pessoas" />
+        <PeopleList peoples={this.state.peoples} />
+        <StatusBar style="auto" />
+      </View>
+    )
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
